c8ab2e1a0c57d3082e8de6c2febb5a8e89dcb456
emexSW <jonsons.hub@gmail.com>
2025-10-30 18:38:43 +0100

    [run] run_qemu.sh
    Script to run QEMU with specified ISO file.

---
+++ run_qemu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ISO="$1"
+
+if [ -z "$ISO" ]; then
+    echo "Error: No ISO filename provided"
+    exit 1
+fi
+
+echo "  [QEMU]  $ISO"
+
+qemu-system-x86_64 \
+    -cdrom "$ISO" \
+    -m 256M \
+    \
+    # -enable-kvm \
+    \
+    # -device qemu-xhci,id=xhci \
+    # -device usb-kbd \
+    # -device usb-mouse \
+    \
+2>/dev/null || \
+qemu-system-x86_64 \
+    -cdrom "$ISO" \
+    -m 256M \
+    \
+    # -device qemu-xhci,id=xhci \
+    # -device usb-kbd \
+    # -device usb-mouse \
