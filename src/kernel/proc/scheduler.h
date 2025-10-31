eec55a6a9bed3e53230fd66e3f7d4005339d9393
emexSW <jonsons.hub@gmail.com>
2025-10-31 18:49:34 +0100

    [MEM, PROC] Refactor memory & scheduler
    Refactor scheduler functions and add scheduling queue structure.

--- src/kernel/proc/scheduler.h
+++ src/kernel/proc/scheduler.h
@@ -2,10 +2,23 @@
 #define SCHEDULER_H
 
 #include "../../../shared/types.h"
+#include "process.h"
 
-// Basic scheduler functions (not used yet)
-void scheduler_init(void);
-void scheduler_tick(void);
-int scheduler_is_enabled(void);
+typedef struct {
+    proc_t *queue[32];
+    int head, tail, cnt;
+} sched_queue_t;
+
+
+// Basic scheduler functions
+void sched_init(void);
+void sched_enable(void);
+void sched_disable(void);
+int sched_is_enabled(void);
+
+void sched_add(proc_t *p);
+void sched_remove(proc_t *p);
+void sched_tick(void);
+void sched_yield(void);
 
 #endif
