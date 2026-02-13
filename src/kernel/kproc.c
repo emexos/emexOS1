66842ae3f127fb4e852a7ce81b326880acd02abd
emexos <jonsons.hub@gmail.com>
2026-02-13 22:11:50 +0100

    [KERN] kernel processes, fixed init issue

--- src/kernel/kproc.c
+++ src/kernel/kproc.c
@@ -7,6 +7,8 @@
 #include <config/system.h>
 
 /*
+// create a process for the kernel
+// if proc: kernel == quit the system stops/halt/crashes idk
 void create_kernel_process(ulime_t *ulime, proc_manager_t *proc_mgr)
 {
     ulime_proc_t *kernel_proc = ulime_proc_create(
@@ -19,7 +21,7 @@ void create_kernel_process(ulime_t *ulime, proc_manager_t *proc_mgr)
     kernel_proc->state = PROC_READY;
     kernel_proc->is_critical = 1;
 
-    // in ProcManager registrieren
+    // register process manager
     proc_add(proc_mgr, kernel_proc);
 
     log("[KPROC]", "Kernel process created\n", 0);
